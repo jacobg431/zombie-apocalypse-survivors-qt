328f4c23047edbecc1f1d85c0db3d0c821a1510e
magnuax <magaxelsen@gmail.com>
2026-02-17 11:10:13 +0100

    UPDATE: add global styles

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,32 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    int fontId = QFontDatabase::addApplicationFont(":/resources/fonts/DoubleHomicide.ttf");
+    // --- Global styling ---
+    app.setStyleSheet(R"(
+        QMainWindow { background-color: #2E2E2E; }
+
+        QLabel { color: white; }
+
+        QPushButton {
+            background-color: #444;
+            color: white;
+            border: 4px solid #666;
+            padding: 8px;
+            border-radius: 20px;
+            qproperty-flat: false;
+        }
+
+        QPushButton:hover {
+            background-color: #555;
+        }
+
+        QPushButton:pressed {
+            background-color: #333;
+        }
+    )");
+
+    int fontId = QFontDatabase::addApplicationFont(":/resources/fonts/alagard.ttf");
+
     try
     {
         const auto families = QFontDatabase::applicationFontFamilies(fontId);
@@ -16,6 +41,8 @@ int main(int argc, char *argv[])
     {
         qWarning() << "Failed to load font from resource";
     }
+
+    // --- Main window ---
 
     AppWindow window;
     window.show();
