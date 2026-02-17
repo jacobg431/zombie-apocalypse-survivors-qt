7d971844ee577d1196ea982ba9dfcd8cba2493f8
magnuax <magaxelsen@gmail.com>
2026-02-17 10:31:30 +0100

    UPDATE: change font

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <QApplication>
 #include "AppWindow.hpp"
+#include <QFontDatabase>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    int fontId = QFontDatabase::addApplicationFont(":/resources/fonts/DoubleHomicide.ttf");
+    try
+    {
+        const auto families = QFontDatabase::applicationFontFamilies(fontId);
+        app.setFont(QFont(families.first()));
+    }
+    catch (...)
+    {
+        qWarning() << "Failed to load font from resource";
+    }
 
     AppWindow window;
     window.show();
