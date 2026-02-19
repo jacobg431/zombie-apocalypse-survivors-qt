b8598d6503f060303fb080bf589d2b6f6772d30b
magnuax <magaxelsen@gmail.com>
2026-02-19 15:51:09 +0100

    UPDATE: change text glitch animation time

--- include/utils.hpp
+++ include/utils.hpp
@@ -8,4 +8,4 @@ QPixmap makeGlitchFrame(const QPixmap &src);
 
 QString glitchify(const QString &target, double progress);
 
-void setGlitchText(QLabel *label, const QString &finalText, int durationMs = 600, int intervalMs = 40);
+void setGlitchText(QLabel *label, const QString &finalText, int durationMs = 200, int intervalMs = 20);
