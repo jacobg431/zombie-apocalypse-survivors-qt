0ab24430fd58989766ee6582d0c444d8ca72654d
Jacob Gomez Hansen <jacobg431@gmail.com>
2026-02-26 13:45:46 +0100

    UPDATE: Changed PlayerState properties to comply with Survivor properties

--- include/models/SessionStateModel.hpp
+++ include/models/SessionStateModel.hpp
@@ -1,13 +1,22 @@
 #pragma once
 
-#include <QString>
 #include <memory>
+
+#include <QString>
+#include <QStringList>
 
 struct PlayerState
 {
     QString characterName;
     QString className;
-    int level = 1;
-    int health = 100;
-    int experience = 0;
+    QString description;
+    QStringList skills = {};
+
+    int strength = 0;
+    int endurance = 0;
+    int agility = 0;
+    int courage = 0;
+    int intelligence = 0;
+    int leadership = 0;
+    int trustworthiness = 0;
 };
